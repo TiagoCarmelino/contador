0aafcb5d6aa1cb85c85dfe58deadae25a400efa9
TiagoCarmelino <tiagocarmelino@hotmail.com>
2024-03-26 21:24:43 -0300

    Contador de Pessoas

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -6,56 +5,115 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: HomePage(),
     );
   }
 }
 
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  HomePageState createState() => HomePageState();
+}
+
+class HomePageState extends State<HomePage> {
+  int peopleCount = 0;
+
+  void decrement() {
+    setState(() {
+      if (peopleCount > 0) {
+        peopleCount--;
+      }
+    });
+  }
+
+  void increment() {
+    setState(() {
+      if (peopleCount < 20) {
+        peopleCount++;
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          const Text(
-            'Pode Entrar!',
-            style: TextStyle(
-              fontSize: 30,
-              color: Colors.white,
-              fontWeight: FontWeight.w700,
-            ),
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/imgs/Gran_Turismo_4_capa.png'),
+            fit: BoxFit.cover,
           ),
-          const Text(
-            '0',
-            style: TextStyle(
-              fontSize: 100,
-              color: Colors.black,
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              peopleCount == 20 ? 'Lotado!' : 'Pode Entrar!',
+              style: TextStyle(
+                fontSize: peopleCount == 20 ? 40 : 30,
+                color: peopleCount == 20 ? Colors.red : Colors.blue,
+                fontWeight: FontWeight.w700,
+              ),
             ),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              TextButton(
-                onPressed: null,
-                child: Text(
-                  'Saiu',
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 16,
+            Text(
+              '$peopleCount',
+              style: const TextStyle(
+                fontSize: 100,
+                color: Colors.black,
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextButton(
+                  onPressed: peopleCount == 0 ? null : decrement,
+                  style: TextButton.styleFrom(
+                    backgroundColor: Colors.red,
+                    minimumSize: const Size(100, 100),
+                    foregroundColor: Colors.black,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(24),
+                    ),
+                  ),
+                  child: const Text(
+                    'Saiu',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 16,
+                    ),
                   ),
                 ),
-              ),
-            ],
-          ),
-        ],
+                const SizedBox(width: 32),
+                TextButton(
+                  onPressed: peopleCount == 20 ? null : increment,
+                  style: TextButton.styleFrom(
+                    backgroundColor: Colors.red,
+                    minimumSize: const Size(100, 100),
+                    foregroundColor: Colors.black,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(24),
+                    ),
+                  ),
+                  child: const Text(
+                    'Entrou',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 16,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
